a9651f4c94ed8892ea6dc5db8e2177019ac83c62
Devernua <kukuev@phystech.edu>
2016-12-18 23:12:22 +0300

    add get train and check

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
-#coding: utf-8
+# coding: utf-8
 from . import views
 from django.conf.urls import url
 
 urlpatterns = [
-		url(r'^$', views.get_json, name='get_json'),
+    url(r'^$', views.get_json, name='get_json'),
+    url(r'^train$', views.get_train, name="get_train"),
+    url(r'^check$', views.get_check, name="get_check"),
 ]
--- api/views.py
+++ api/views.py
@@ -2,17 +2,48 @@ from django.shortcuts import render
 from django.http import JsonResponse, Http404
 import json
 
-def get_json(request):
-	try:
-	    requestJson = json.loads(request.body.decode())
-	except Exception:
-		return JsonResponse({"error": 404 })
-	#else:
-	#    return JsonResponse({"error": 404 })
-	#print(request.body)
 
-	if (request.method != "POST" or requestJson is None):
-		return JsonResponse({"error": 404 })
-	#TODO: something with json
-	return JsonResponse(requestJson)
+def get_json(request):
+    try:
+        requestJson = json.loads(request.body.decode())
+    except Exception:
+        return JsonResponse({"error": 404})
+    #else:
+    #    return JsonResponse({"error": 404 })
+    #print(request.body)
+
+    if (request.method != "POST" or requestJson is None):
+        return JsonResponse({"error": 404})
+    #TODO: something with json
+    return JsonResponse(requestJson)
+
+
+def get_check(request):
+    try:
+        requestJson = json.loads(request.body.decode())
+    except Exception:
+        return JsonResponse({"error": 404})
+    # else:
+    #    return JsonResponse({"error": 404 })
+    # print(request.body)
+
+    if (request.method != "POST" or requestJson is None):
+        return JsonResponse({"error": 404})
+    # TODO: something with json
+    return JsonResponse(requestJson)
+
+
+def get_train(request):
+    try:
+        requestJson = json.loads(request.body.decode())
+    except Exception:
+        return JsonResponse({"error": 404})
+    # else:
+    #    return JsonResponse({"error": 404 })
+    # print(request.body)
+
+    if (request.method != "POST" or requestJson is None):
+        return JsonResponse({"error": 404})
+    # TODO: something with json
+    return JsonResponse(requestJson)
 
